e0cd01dfc34fd3e27ea817cfd37ab79bf971d869
unknown <teddy.kalp@gmail.com>
2021-11-30 10:47:07 -0700

    added lottery code

---
+++ src/lottery.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+
+// Address where contract is deployed
+const address = '0xB63267624C8ecB85BB2f7896a9Aa1D98FE7E6bB9';
+// Our interface
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+// Exporting ethereum LOCAL contract
+export default new web3.eth.Contract(abi, address);
